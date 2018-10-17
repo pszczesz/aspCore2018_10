2403ece558ccdf3cd899c0fceb91c59c10d641ce
pszczesz <pszczesz@gmail.com>
2018-10-17 15:30:59 +0200

    day 1

---
+++ MyFirstCore/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyFirstCore.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options)
+            :base(options)
+        {
+            
+        }
+        public  DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ MyFirstCore/Pages/index2.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFirstCore.Pages
+{
+    public class index2Model : PageModel {
+        public string Message { get; private set; } = "Page model in C#";
+        public void OnGet() {
+            Message += $"Server time is {DateTime.Now}";
+        }
+    }
+}
